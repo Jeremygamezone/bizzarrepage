318dd6218775eea98c7ded8899beab4b585566d7
Jeremygamezone <93847223+Jeremygamezone@users.noreply.github.com>
2026-03-13 08:15:43 -0600

    amonas

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import icon from "astro-icon";
-import github from '@astrojs/github-pages';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeremygamezone.github.io',
   base: '/bizzarrepage/',
-  integrations: [tailwind(), react(), vue(), icon(),github()]
+  integrations: [tailwind(), react(), vue(), icon()]
 });
