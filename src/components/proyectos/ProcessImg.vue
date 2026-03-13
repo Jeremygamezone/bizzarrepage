55bcb8b66a169532890b026c9931c4ccb8736c8b
Jeremygamezone <93847223+Jeremygamezone@users.noreply.github.com>
2026-03-13 16:59:52 -0600

    Display Images
    Cambié el componente de Process Image para que la ruta acceda directamente de src

--- src/components/proyectos/ProcessImg.vue
+++ src/components/proyectos/ProcessImg.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="my-auto py-6">
-        <img :src="`/public/brands/${img}`" alt=""
+        <img :src="imgName" alt=""
             class="rounded-3xl my-auto mx-auto w-56 h-56 object-cover shadow-lg">
+
         <div class="p-8 text-white uppercase">
             <h3 class="text-2xl p-4 font-bold">{{ title }}</h3>
             <p class="text-justify">{{ description }}</p>
@@ -10,10 +11,8 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
-
 const props = defineProps({
-    img: {
+    imgName: {
         type: String,
         required: true
     },
